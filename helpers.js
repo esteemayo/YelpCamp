9c45f7d99426a53e7ea598db03f33499e99ca9fe
Emmanuel Adebayo <eadebayo15@gmail.com>
2020-12-08 06:33:02 +0100

    Added helpers.js file to the project

---
+++ helpers.js
@@ -0,0 +1,9 @@
+
+// moment.js is a handy library for displaying dates. We need this in our templates to display things like "Posted 5 minutes ago"
+exports.moment = require('moment');
+
+// Dump is a handy debugging function we can use to sort of "console.log" our data
+exports.dump = obj => JSON.stringify(obj, null, 2);
+
+// Some details about the website
+exports.siteName = 'YELPCAMP';
