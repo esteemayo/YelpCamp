dbe276d6e57a2f1cbdc08c3cbb34cf854b3d5740
Emmanuel Adebayo <eadebayo15@gmail.com>
2020-12-09 20:51:41 +0100

    Refactored the middlewares

--- middleware/index.js
+++ middleware/index.js
@@ -1,69 +1,60 @@
+const catchErrors = require('../utils/catchErrors');
 const Campground = require('../models/Campgrounds');
 const Comment = require('../models/Comment');
 const User = require('../models/User');
 
 let middleware = {};
 
-middleware.checkCampgroundOwnership = function (req, res, next) {
+middleware.checkCampgroundOwnership = catchErrors(async (req, res, next) => {
     if (req.isAuthenticated()) {
-        Campground.findById(req.params.id, (err, foundCampground) => {
-            if (err) {
-                res.flash('error', 'Campground not found');
-                res.redirect('back');
-            } else {
-                if (foundCampground.author.id.equals(req.user._id) || req.user.isAdmin) {
-                    next();
-                } else {
-                    req.flash('error', 'You don\'t have permission to do that');
-                    res.redirect('back');
-                }
-            }
-        });
+        const campground = await Campground.findById(req.params.id);
+
+        if (!campground) {
+            req.flash('error', 'Campground not found');
+            return res.redirect('back');
+        }
+
+        if (campground.author.id.equals(req.user._id) || req.user.isAdmin) return next();
+
+        req.flash('error', 'You don\'t have permission to do that');
+        res.redirect('back');
     } else {
         req.flash('error', 'You need to be logged in to do that');
         res.redirect('back');
     }
-}
+});
 
-middleware.checkCommentOwnership = function (req, res, next) {
+middleware.checkCommentOwnership = catchErrors(async (req, res, next) => {
     if (req.isAuthenticated()) {
-        Comment.findById(req.params.comment_id, (err, foundComment) => {
-            if (err) {
-                res.redirect('back');
-            } else {
-                if (foundComment.author.id.equals(req.user._id) || req.user.isAdmin) {
-                    next();
-                } else {
-                    req.flash('error', 'You don\'t have permission to do that');
-                    res.redirect('back');
-                }
-            }
-        });
+        const comment = await Comment.findById(req.params.comment_id);
+
+        if (!comment) {
+            return res.redirect('back');
+        }
+
+        if (comment.author.id.equals(req.user._id) || req.user.isAdmin) return next();
+
+        req.flash('error', 'You don\'t have permission to do that');
+        res.redirect('back');
     } else {
         req.flash('error', 'You need to be logged in to do that');
         res.redirect('back');
     }
-}
+});
 
-middleware.isLoggedIn = function (req, res, next) {
+middleware.isLoggedIn = (req, res, next) => {
     if (req.isAuthenticated()) return next();
 
     req.flash('error', 'You need to be logged in to do that');
     res.redirect('/auth/login');
 }
 
-middleware.isNotVerified = async (req, res, next) => {
-    try {
+middleware.isNotVerified = catchErrors(async (req, res, next) => {
         const user = await User.findOne({ username: req.body.username });
         if (user.isVerified) return next();
 
         req.flash('error', 'Your account has not been verified. Please check your email to verify your account!');
         return res.redirect('/');
-    } catch (err) {
-        console.log(err);
-        req.flash('error', 'Something went wrong. Please contact us for assistance!');
-        res.redirect('/');
-    }
-}
+});
 
 module.exports = middleware;
