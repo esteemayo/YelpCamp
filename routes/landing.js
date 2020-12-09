a6a89cbf227a87952cd449d39988c3cae78dfc43
Emmanuel Adebayo <eadebayo15@gmail.com>
2020-12-09 01:04:11 +0100

    Modified landing.js

--- routes/landing.js
+++ routes/landing.js
@@ -1,10 +1,12 @@
 const express = require('express');
+
 const router = express.Router();
 
-// LANDING PAGE ROUTE
+// Landing page route
 router.get('/', (req, res) => {
-    res.render('landing');
+    res.status(200).render('landing', {
+        title: 'Home Page'
+    });
 });
-
 
 module.exports = router;
