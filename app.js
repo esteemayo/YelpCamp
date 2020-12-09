d9fd0b798789a700997d1babc6d73707b61b441e
Emmanuel Adebayo <eadebayo15@gmail.com>
2020-12-09 01:50:04 +0100

    Modified

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
-require('dotenv').config();
 const express = require('express');
 const seedDB = require('./seeds');
-
 
 const app = express();
 
 require('./startup/routes')(app);
-require('./startup/db')();
-
 // seedDB(); seed the DB
 
-
-
-
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => console.log(`YELPCAMP SERVER LISTENING ON PORT ${PORT}`));
+module.exports = app;
