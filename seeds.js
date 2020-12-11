bdf1884cd6ff81e52ba3cb690a2cb14d7a75b726
Emmanuel Adebayo <eadebayo15@gmail.com>
2020-12-11 02:53:46 +0100

    Refactored seeds.js

--- seeds.js
+++ seeds.js
@@ -1,59 +1,66 @@
-const mongoose = require('mongoose');
-const Campground = require('./models/Campgrounds');
+const Campground = require('./models/Campground');
 const Comment = require('./models/Comment');
 
-let data = [
+const data = [
     {
         name: 'Cloud\'s Rest',
-        image: 'https://farm9.staticflickr.com/8041/7930201874_6c17ed670a.jpg',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, eveniet aliquam, deserunt pariatur nesciunt quae possimus consequuntur id tempore adipisci, excepturi reiciendis vitae saepe mollitia! Hic ex quasi ad suscipit Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, eveniet aliquam, deserunt pariatur nesciunt quae possimus consequuntur id tempore adipisci, excepturi reiciendis vitae saepe mollitia! Hic ex quasi ad suscipit'
+        image: 'https://res.cloudinary.com/learnhowtocode/image/upload/v1571104211/ih2inp04yrbimbqf4hyg.jpg',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, eveniet aliquam, deserunt pariatur nesciunt quae possimus consequuntur id tempore adipisci, excepturi reiciendis vitae saepe mollitia! Hic ex quasi ad suscipit Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, eveniet aliquam, deserunt pariatur nesciunt quae possimus consequuntur id tempore adipisci, excepturi reiciendis vitae saepe mollitia! Hic ex quasi ad suscipit',
+        price: 15,
+        author: {
+            id: '5fc4eeadccbb8a30bc4985f3',
+            username: 'jdoe'
+        }
     },
 
     {
         name: 'Desert Mesa',
-        image: 'https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, eveniet aliquam, deserunt pariatur nesciunt quae possimus consequuntur id tempore adipisci, excepturi reiciendis vitae saepe mollitia! Hic ex quasi ad suscipit Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, eveniet aliquam, deserunt pariatur nesciunt quae possimus consequuntur id tempore adipisci, excepturi reiciendis vitae saepe mollitia! Hic ex quasi ad suscipit'
+        image: 'https://res.cloudinary.com/learnhowtocode/image/upload/v1607300637/de1ngzlxy5anjlzyuiqn.jpg',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, eveniet aliquam, deserunt pariatur nesciunt quae possimus consequuntur id tempore adipisci, excepturi reiciendis vitae saepe mollitia! Hic ex quasi ad suscipit Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, eveniet aliquam, deserunt pariatur nesciunt quae possimus consequuntur id tempore adipisci, excepturi reiciendis vitae saepe mollitia! Hic ex quasi ad suscipit',
+        price: 10,
+        author: {
+            id: '5fc4efc5ccbb8a30bc4985f4',
+            username: 'mdoe'
+        }
     },
 
     {
         name: 'Canyon Floor',
-        image: 'https://farm8.staticflickr.com/7338/9627572189_12dbd88ebe.jpg',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, eveniet aliquam, deserunt pariatur nesciunt quae possimus consequuntur id tempore adipisci, excepturi reiciendis vitae saepe mollitia! Hic ex quasi ad suscipit Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, eveniet aliquam, deserunt pariatur nesciunt quae possimus consequuntur id tempore adipisci, excepturi reiciendis vitae saepe mollitia! Hic ex quasi ad suscipit'
+        image: 'https://res.cloudinary.com/learnhowtocode/image/upload/v1607340065/i8qppdjbzox7qrgx3a6j.jpg',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, eveniet aliquam, deserunt pariatur nesciunt quae possimus consequuntur id tempore adipisci, excepturi reiciendis vitae saepe mollitia! Hic ex quasi ad suscipit Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, eveniet aliquam, deserunt pariatur nesciunt quae possimus consequuntur id tempore adipisci, excepturi reiciendis vitae saepe mollitia! Hic ex quasi ad suscipit',
+        price: 13,
+        author: {
+            id: '5fcd31c40748a00ae49850f4',
+            username: 'jennifer'
+        }
     }
 ];
 
-function seedDB() {
-    // remove all campgrounds
-    Campground.deleteMany({}, (err) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log('removed campgrounds!');
-            // add a few campgrounds
-            data.forEach(seeds => {
-                Campground.create(seeds, (err, campground) => {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log('added a campground');
-                        // add a few comments
-                        Comment.create({
-                            text: 'This place is great but, I wish there was internet',
-                            author: 'Homer'
-                        }, (err, comment) => {
-                            if (err) {
-                                console.log(err);
-                            } else {
-                                campground.comments.push(comment);
-                                campground.save();
-                                console.log('Created new comment');
-                            }
-                        })
-                    }
-                });
+async function seedDB() {
+    try {
+        // remove all campgrounds
+        await Campground.deleteMany();
+        console.log('Removed campgrounds!');
+
+        await Comment.deleteMany();
+        console.log('Removed comments!');
+
+        data.forEach(async seeds => {
+            const campground = await Campground.create(seeds);
+            console.log('Added a campground...');
+
+            const comment = await Comment.create({
+                text: 'This place is great but, I wish there was internet',
+                author: 'Homer'
             });
-        }
-    });
+
+            campground.comments.push(comment);
+            await campground.save();
+            console.log('Created new comment...');
+        });
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 module.exports = seedDB;
