b13c9162dcda0a313fe33393c32b8c63355999eb
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-06-28 23:30:07 +0100

    refactored

--- models/Campground.js
+++ models/Campground.js
@@ -1,67 +1,76 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 
-const campgroundSchema = new mongoose.Schema({
+const campgroundSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: [true, 'A campground must have a name'],
-        trim: true,
-        maxlength: [50, 'A campground name must have less or equal than 50 characters'],
-        minlength: [5, 'A campground name must have more or equal than 5 characters']
+      type: String,
+      required: [true, 'A campground must have a name'],
+      trim: true,
+      maxlength: [
+        50,
+        'A campground name must have less or equal than 50 characters',
+      ],
+      minlength: [
+        5,
+        'A campground name must have more or equal than 5 characters',
+      ],
     },
     price: {
-        type: Number,
-        required: [true, 'A campground must have a price']
+      type: Number,
+      required: [true, 'A campground must have a price'],
     },
     slug: String,
     image: String,
     imageId: String,
     description: {
-        type: String,
-        trim: true
+      type: String,
+      trim: true,
     },
     location: String,
     lat: Number,
     lng: Number,
     author: {
-        id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User',
-            required: [true, 'A campground must belong to an author.']
-        },
-        username: String
+      id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'A campground must belong to an author.'],
+      },
+      username: String,
     },
     comments: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Comment'
-        }
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment',
+      },
     ],
     date: {
-        type: Date,
-        default: Date.now
-    }
-}, {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
     toJSON: { virtuals: true },
-    toObject: { virtuals: true }
-});
+    toObject: { virtuals: true },
+  }
+);
 
 campgroundSchema.index({ name: 1, price: 1 });
 campgroundSchema.index({ slug: 1 });
 
 campgroundSchema.pre('save', async function (next) {
-    if (!this.isModified('name')) return next();
+  if (!this.isModified('name')) return next();
 
-    this.slug = slugify(this.name, { lower: true });
+  this.slug = slugify(this.name, { lower: true });
 
-    const slugRegExp = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
-    const campgroundWithSlug = await this.constructor.find({ slug: slugRegExp });
+  const slugRegExp = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+  const campgroundWithSlug = await this.constructor.find({ slug: slugRegExp });
 
-    if (campgroundWithSlug.length) {
-        this.slug = `${this.slug}-${campgroundWithSlug.length + 1}`;
-    }
+  if (campgroundWithSlug.length) {
+    this.slug = `${this.slug}-${campgroundWithSlug.length + 1}`;
+  }
 
-    next();
+  next();
 });
 
 const Campground = mongoose.model('Campground', campgroundSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -1,29 +1,25 @@
 const express = require('express');
+
 const { isNotVerified } = require('../middleware/index');
 const authController = require('../controller/authController');
 
 const router = express.Router();
 
-// Show login form && handling login logic
 router
-    .route('/login')
-    .get(authController.loginForm)
-    .post(
-        isNotVerified,
-        authController.login
-    );
+  .route('/login')
+  .get(authController.loginForm)
+  .post(isNotVerified, authController.login);
 
-// Logout route
 router.get('/logout', authController.logout);
 
 router
-    .route('/forgot')
-    .get(authController.forgotForm)
-    .post(authController.forgotPassword);
+  .route('/forgot')
+  .get(authController.forgotForm)
+  .post(authController.forgotPassword);
 
 router
-    .route('/reset/:token')
-    .get(authController.reset)
-    .post(authController.resetPassword);
+  .route('/reset/:token')
+  .get(authController.reset)
+  .post(authController.resetPassword);
 
 module.exports = router;
