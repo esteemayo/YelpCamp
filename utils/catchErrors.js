cc6de7e1db5fe1f4c3362c0d08e1270295d77dde
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-06-28 23:29:39 +0100

    modified

--- utils/catchErrors.js
+++ utils/catchErrors.js
@@ -1,7 +1,5 @@
-
-
-module.exports = fn => {
-    return (req, res, next) => {
-        return fn(req, res, next).catch(next);
-    }
-}
+module.exports = (fn) => {
+  return (req, res, next) => {
+    return fn(req, res, next).catch(next);
+  };
+};
