c0d3afa9e8d451f51885236c61422b8cfbd8ba50
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-06-28 23:29:52 +0100

    modified

--- routes/forgot.js
+++ routes/forgot.js
@@ -1,68 +1,81 @@
-const express = require('express');
-const router = express.Router();
 const async = require('async');
 const crypto = require('crypto');
-const nodemailer = require('nodemailer');
-const User = require('../models/User');
+const express = require('express');
 
-// FORGOT PASSWORD ROUTE
+const User = require('../models/User');
+const nodemailer = require('nodemailer');
+
+const router = express.Router();
+
 router.get('/', (req, res) => {
-    res.render('forgot');
+  res.render('forgot');
 });
 
 router.post('/', (req, res, next) => {
-    async.waterfall([
-        function (done) {
-            crypto.randomBytes(20, (err, buf) => {
-                let token = buf.toString('hex');
-                done(err, token);
-            });
-        },
+  async.waterfall(
+    [
+      function (done) {
+        crypto.randomBytes(20, (err, buf) => {
+          let token = buf.toString('hex');
+          done(err, token);
+        });
+      },
 
-        function (token, done) {
-            User.findOne({ email: req.body.email }, (err, user) => {
-                if (!user) {
-                    req.flash('error', 'No account with that email address exists.');
-                    return res.redirect('/forgot');
-                }
-                user.resetPasswordToken = token;
-                user.resetPasswordExpires = Date.now() + 3600000; // 1 Hour
+      function (token, done) {
+        User.findOne({ email: req.body.email }, (err, user) => {
+          if (!user) {
+            req.flash('error', 'No account with that email address exists.');
+            return res.redirect('/forgot');
+          }
+          user.resetPasswordToken = token;
+          user.resetPasswordExpires = Date.now() + 3600000; // 1 Hour
 
-                user.save(err => {
-                    done(err, token, user);
-                });
-            });
-        },
+          user.save((err) => {
+            done(err, token, user);
+          });
+        });
+      },
 
-        function (token, user, done) {
-            let smtpTransporter = nodemailer.createTransport({
-                service: process.env.MAIL_SERVICE,
-                auth: {
-                    user: process.env.CLIENT_MAIL,
-                    pass: process.env.CLIENT_PASSWORD
-                }
-            });
+      function (token, user, done) {
+        let smtpTransporter = nodemailer.createTransport({
+          service: process.env.MAIL_SERVICE,
+          auth: {
+            user: process.env.CLIENT_MAIL,
+            pass: process.env.CLIENT_PASSWORD,
+          },
+        });
 
-            let mailOptions = {
-                to: user.email,
-                from: process.env.CLIENT_MAIL,
-                subject: 'YelpCamp Password Reset',
-                text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
-                    'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-                    'http://' + req.headers.host + '/reset/' + token + '\n\n' +
-                    'If you did not request this, please ignore this email and your password will remain unchanged.\n'
-            }
+        let mailOptions = {
+          to: user.email,
+          from: process.env.CLIENT_MAIL,
+          subject: 'YelpCamp Password Reset',
+          text:
+            'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+            'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+            'http://' +
+            req.headers.host +
+            '/reset/' +
+            token +
+            '\n\n' +
+            'If you did not request this, please ignore this email and your password will remain unchanged.\n',
+        };
 
-            smtpTransporter.sendMail(mailOptions, err => {
-                req.flash('success', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
-                done(err, 'done');
-            });
-        }
-    ], function (err) {
-        if (err) return next(err);
-        res.redirect('/forgot');
-    });
+        smtpTransporter.sendMail(mailOptions, (err) => {
+          req.flash(
+            'success',
+            'An e-mail has been sent to ' +
+              user.email +
+              ' with further instructions.'
+          );
+          done(err, 'done');
+        });
+      },
+    ],
+    function (err) {
+      if (err) return next(err);
+      res.redirect('/forgot');
+    }
+  );
 });
-
 
 module.exports = router;
--- routes/register.js
+++ routes/register.js
@@ -1,29 +1,30 @@
 const express = require('express');
+
+const { isLoggedIn } = require('../middleware/index');
 const userController = require('../controller/userController');
 const imageController = require('../controller/imageController');
-const { isLoggedIn } = require('../middleware/index');
 
 const router = express.Router();
 
-// Show register form && handle sign up logic
 router
-    .route('/register')
-    .get(userController.registerForm)
-    .post( 
-        imageController.upload,
-        imageController.resize,
-        userController.register
-    );
+  .route('/register')
+  .get(userController.registerForm)
+  .post(
+    imageController.upload,
+    imageController.resize,
+    userController.register
+  );
 
 router.get('/register/verify-email', userController.verifyEmail);
 
 router.get('/account/me', isLoggedIn, userController.account);
 
-router.post('/submit-user-data',
-    isLoggedIn,
-    imageController.upload,
-    imageController.resize,
-    userController.updateUserData
+router.post(
+  '/submit-user-data',
+  isLoggedIn,
+  imageController.upload,
+  imageController.resize,
+  userController.updateUserData
 );
 
 router.get('/:username', userController.userProfile);
