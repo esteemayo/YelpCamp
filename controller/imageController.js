0fc9502449f5949bf863b397d7b294c17aa1ffc9
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-06-28 23:30:16 +0100

    modified

--- controller/imageController.js
+++ controller/imageController.js
@@ -1,35 +1,36 @@
 const uuid = require('uuid');
 const sharp = require('sharp');
 const multer = require('multer');
+
 const catchErrors = require('../utils/catchErrors');
 
 const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
-    if (file.mimetype.startsWith('image/')) {
-        return cb(null, true);
-    }
-    return cb(new Error('Only image files are allowed'), false);
+  if (file.mimetype.startsWith('image/')) {
+    return cb(null, true);
+  }
+  return cb(new Error('Only image files are allowed'), false);
 };
 
 const upload = multer({
-    storage: multerStorage,
-    fileFilter: multerFilter
+  storage: multerStorage,
+  fileFilter: multerFilter,
 });
 
 exports.upload = upload.single('avatar');
 
 exports.resize = catchErrors(async (req, res, next) => {
-    if (!req.file) return next();
+  if (!req.file) return next();
 
-    const ext = req.file.mimetype.split('/')[1];
-    req.file.filename = `${uuid.v4()}.${ext}`;
+  const ext = req.file.mimetype.split('/')[1];
+  req.file.filename = `${uuid.v4()}.${ext}`;
 
-    await sharp(req.file.buffer)
-        .resize(500, 500)
-        .toFormat('jpeg')
-        .jpeg({ quality: 90 })
-        .toFile(`public/uploads/users/${req.file.filename}`);
+  await sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/uploads/users/${req.file.filename}`);
 
-    next();
+  next();
 });
--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,7 @@
 const _ = require('lodash');
 const crypto = require('crypto');
+const { StatusCodes } = require('http-status-codes');
+
 const User = require('../models/User');
 const sendMail = require('../utils/mail');
 const AppError = require('../utils/appError');
@@ -7,122 +9,140 @@ const Campground = require('../models/Campground');
 const catchErrors = require('../utils/catchErrors');
 
 exports.registerForm = (req, res) => {
-    if (req.isAuthenticated()) return res.redirect('/');
+  if (req.isAuthenticated()) return res.redirect('/');
 
-    res.status(200).render('register', {
-        title: 'Register your account!'
-    });
-}
+  res.status(StatusCodes.OK).render('register', {
+    title: 'Register your account!',
+  });
+};
 
 exports.register = catchErrors(async (req, res, next) => {
-    let avatar;
-    if (req.file) avatar = req.file.filename;
+  let avatar;
+  if (req.file) avatar = req.file.filename;
 
-    const newUser = new User({
-        username: req.body.username,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        emailToken: crypto.randomBytes(64).toString('hex'),
-        avatar
-    });
-    
-    if (req.body.adminCode === 'secretcode123') {
-        newUser.isAdmin = true;
+  const newUser = new User({
+    username: req.body.username,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    emailToken: crypto.randomBytes(64).toString('hex'),
+    avatar,
+  });
+
+  if (req.body.adminCode === 'secretcode123') {
+    newUser.isAdmin = true;
+  }
+  User.register(newUser, req.body.password, async (err, user) => {
+    if (err) {
+      req.flash('error', err.message);
+      return res.status(StatusCodes.OK).render('register', {
+        title: 'Create your account!',
+      });
     }
-    User.register(newUser, req.body.password, async (err, user) => {
-        if (err) {
-            req.flash('error', err.message);
-            return res.status(200).render('register', {
-                title: 'Create your account!'
-            });
-        }
 
-        try {
-            await sendMail({
-                email: user.email,
-                subject: 'YelpCamp - verify your email',
-                text: `
-                    Hello, thanks for registering on our site.
-                    Please copy and paste the address below to verify your account
-                    ${req.protocol}://${req.get('host')}/users/register/verify-email?token=${user.emailToken}
-                `,
-                html: `
-                    <h1>Hello,</h1>
-                    <p>Thanks for registering on our site.</p>
-                    <p>Please click the link below to verify your account.</p>
-                    <a href="${req.protocol}://${req.get('host')}/users/register/verify-email?token=${user.emailToken}">Verify your account</a>
-                `
-            });
+    try {
+      await sendMail({
+        email: user.email,
+        subject: 'YelpCamp - verify your email',
+        text: `
+          Hello, thanks for registering on our site.
+          Please copy and paste the address below to verify your account
+          ${req.protocol}://${req.get(
+          'host'
+        )}/users/register/verify-email?token=${user.emailToken}
+      `,
+        html: `
+          <h1>Hello,</h1>
+          <p>Thanks for registering on our site.</p>
+          <p>Please click the link below to verify your account.</p>
+          <a href="${req.protocol}://${req.get(
+          'host'
+        )}/users/register/verify-email?token=${
+          user.emailToken
+        }">Verify your account</a>
+      `,
+      });
 
-            req.flash('success', 'Thanks for registering please check your email to verify your account.');
-            res.redirect('/');
-        } catch (err) {
-            req.flash('error', 'Oops! Something went wrong. Please contact us for assistance.');
-            res.redirect('/');
-        }
-    });
+      req.flash(
+        'success',
+        'Thanks for registering please check your email to verify your account.'
+      );
+      res.status(StatusCodes.OK).redirect('/');
+    } catch (err) {
+      req.flash(
+        'error',
+        'Oops! Something went wrong. Please contact us for assistance.'
+      );
+      res.status(StatusCodes.OK).redirect('/');
+    }
+  });
 });
 
 exports.verifyEmail = catchErrors(async (req, res, next) => {
-    const user = await User.findOne({ emailToken: req.query.token });
+  const user = await User.findOne({ emailToken: req.query.token });
 
-    if (!user) {
-        req.flash('error', 'Oops! Token is invalid. Please contact us for assistance.');
-        return res.redirect('/');
-    }
+  if (!user) {
+    req.flash(
+      'error',
+      'Oops! Token is invalid. Please contact us for assistance.'
+    );
+    return res.redirect('/');
+  }
 
-    user.emailToken = undefined;
-    user.isVerified = true;
-    await user.save();
-    await req.logIn(user, err => {
-        if (err) return next(err);
-        req.flash('success', `Welcome to YelpCamp ${user.email}`);
-        const redirectUrl = req.session.redirectTo || '/';
-        delete req.session.redirectTo;
-         res.redirect(redirectUrl);
-    });
+  user.emailToken = undefined;
+  user.isVerified = true;
+  await user.save();
+  await req.logIn(user, (err) => {
+    if (err) return next(err);
+    req.flash('success', `Welcome to YelpCamp ${user.email}`);
+    const redirectUrl = req.session.redirectTo || '/';
+    delete req.session.redirectTo;
+    res.redirect(redirectUrl);
+  });
 });
 
 exports.account = catchErrors(async (req, res, next) => {
-    const campgrounds = await Campground
-        .find()
-        .where('author.id')
-        .equals(req.user._id)
-        .sort('-date');
+  const campgrounds = await Campground.find()
+    .where('author.id')
+    .equals(req.user._id)
+    .sort('-date');
 
-    res.status(200).render('account', {
-        title: 'User account settings',
-        campgrounds
-    });
+  res.status(StatusCodes.OK).render('account', {
+    title: 'User account settings',
+    campgrounds,
+  });
 });
 
 exports.updateUserData = catchErrors(async (req, res, next) => {
-    const filterBody = _.pick(req.body, ['firstName', 'lastName', 'email']);
-    if (req.file) filterBody.avatar = req.file.filename;
+  const filterBody = _.pick(req.body, ['firstName', 'lastName', 'email']);
+  if (req.file) filterBody.avatar = req.file.filename;
 
-    await User.findByIdAndUpdate(req.user._id, filterBody, {
-        new: true,
-        runValidators: true
-    });
+  await User.findByIdAndUpdate(req.user._id, filterBody, {
+    new: true,
+    runValidators: true,
+  });
 
-    req.flash('success', 'Your account data was successfully updated.');
-    res.redirect('/users/account/me');
+  req.flash('success', 'Your account data was successfully updated.');
+  res.status(StatusCodes.OK).redirect('/users/account/me');
 });
 
 exports.userProfile = catchErrors(async (req, res, next) => {
-    const user = await User.findOne({ username: req.params.username });
-    const campgrounds = await Campground
-        .find({ 'author.username': user.username })
-        .sort('-date');
+  const user = await User.findOne({ username: req.params.username }).populate(
+    'followers'
+  );
+  const campgrounds = await Campground.find({
+    'author.username': user.username,
+  }).sort('-date');
 
-    if (!user) {
-        return next(new AppError('No user found with that ID', 400));
-    }
+  if (!user) {
+    return next(
+      new AppError('No user found with that ID', StatusCodes.BAD_REQUEST)
+    );
+  }
 
-    res.status(200).render('users/profile', {
-        title: `${user.fullName}'s profile!`,
-        campgrounds,
-        user
-    });
+  res.status(StatusCodes.OK).render('users/profile', {
+    title: `${user.fullName}'s profile!`,
+    campgrounds,
+    user,
+  });
 });
